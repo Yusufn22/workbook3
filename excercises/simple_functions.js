96002407cd5e249ee1356bda8e5acf80132ddbb3
Yusufn22 <Yusufnaderi24@gmail.com>
2024-04-29 15:18:38 -0400

    simple functions

---
+++ excercises/simple_functions.js
@@ -0,0 +1,21 @@
+function favoriteThings() {
+    console.log("My name is Yusuf Naderi.");
+    console.log("My favorite movie is fast and furious");
+    console.log("my favorite rapper is 50 cent");
+    
+}
+
+
+function whyImHere() {
+    console.log("I joined the learning to code program to expand my knowledge in coding and hopefully land a job");
+    
+}
+
+
+function favoritePlace() {
+    console.log("I love to visit the beach when its hot")
+    
+}
+favoritePlace()
+favoriteThings()
+whyImHere()
