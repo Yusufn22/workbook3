8c262136745e9337c5ef1b7e4e1668f1827ac558
Yusufn22 <Yusufnaderi24@gmail.com>
2024-05-01 13:33:06 -0400

    excercises

---
+++ excercises/1.js
@@ -0,0 +1,40 @@
+"use strict";
+function test1() {
+ let a = 10;
+ if (a > 5) {
+ a = 7;
+ }
+ console.log("a = " + a);
+}
+test1();
+
+"use strict";
+function test2A() {
+ if (1 == 1) {
+ var a = 5;
+ }
+ console.log("a = " + a);
+}
+test2A();
+
+
+"use strict";
+function test2B() {
+ if (1 == 1) {
+ var a = 5;
+ }
+ console.log("a = " + a);
+}
+test2B();
+
+
+"use strict";
+let a = 4;
+function test3() {
+ a = 3;
+ console.log("a = " + a);
+}
+test3();
+console.log("a = " + a);
+
+
---
+++ excercises/parsing.js
@@ -0,0 +1,10 @@
+let name = "Brenda Kaye";
+let positionOfSpace = name.indexOf(" ");
+console.log(positionOfSpace);
+
+let first = name.substring(0,6);
+console.log(first);
+
+let last = name.substring(7,11);
+console.log(last);
+
