325eb742c7f767b7f026804170ab7bf227d337fa
Yusufn22 <Yusufnaderi24@gmail.com>
2024-05-01 14:47:01 -0400

    more excersies

---
+++ dateScripts/more_data_conversions.js
@@ -0,0 +1,17 @@
+"use script"
+let birthday = new Date(2003, 10, 22);
+console.log(birthday.toLocaleDateString());
+
+let momsBirthday = new Date(1978, 6, 23);
+console.log(momsBirthday.toLocaleDateString());
+
+let dadsBirthday = new Date(1962, 5, 14);
+console.log(dadsBirthday.toLocaleDateString());
+
+
+
+
+
+
+
+
